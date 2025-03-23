9768ac53e5e3015bb94abaec806896c9a012520f
Geovana Garcia dos Santos <geovanagrc2005@gmail.com>
2025-03-23 11:45:43 -0300

    Janela configurada

---
+++ Main.py
@@ -0,0 +1,24 @@
+
+import tkinter as tk
+from tkinter import *
+from tkinter.messagebox import showinfo,showerror
+
+# Configurando a janela 
+tela = Tk()
+tela.geometry('500x600')
+tela.resizable(0,0)
+tela.title("Formulário")
+
+#Campo nome
+tNome = Label(tela, text="Nome",background="green").grid(row=0,column=0)
+
+
+
+
+
+
+
+
+
+
+tela.mainloop()
