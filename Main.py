271bd1863d15ec03dac727507da43e6ac7feb5e7
Geovana Garcia dos Santos <geovanagrc2005@gmail.com>
2025-03-23 13:49:21 -0300

    Adicionando Radiobutton de gênero

--- Main.py
+++ Main.py
@@ -9,14 +9,20 @@ tela.geometry('400x500')
 tela.resizable(0,0)
 tela.title("Formulário")
 
-
 #Campo: nome
 tNome = Label(tela,text="Insira um nome:").place(x=0,y=2)
-eNome = Entry(tela, width=30).place(x=0,y=40)
+eNome = Entry(tela, width=30).place(x=20,y=40)
 
 #Campo: e-mail
 tEmail= Label(tela, text="Insira um e-mail:").place(x=0,y=90)
-eEmail= Entry(tela, width=30).place(x=0, y=130)
+eEmail= Entry(tela, width=30).place(x=20, y=130)
+
+#Campo: Genero
+tGenero = Label(text="Gênero").place(x=0,y=180)
+eGenero = StringVar()
+eGenero.set("Null")
+opFeminino = Radiobutton(text="Feminino",variable=eGenero, value="Feminino").place(x=0,y=210) 
+opMasculino = Radiobutton(text="Masculino",variable=eGenero,value="Masculino").place(x=0,y=250)
 
 
 tela.mainloop()
