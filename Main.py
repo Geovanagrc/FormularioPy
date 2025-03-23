c9d3e89710b0205f28def92d2f6e5c22dea0d8dc
Geovana Garcia dos Santos <geovanagrc2005@gmail.com>
2025-03-23 12:08:47 -0300

    Adicionado: Campo nome e email

--- Main.py
+++ Main.py
@@ -5,20 +5,18 @@ from tkinter.messagebox import showinfo,showerror
 
 # Configurando a janela 
 tela = Tk()
-tela.geometry('500x600')
+tela.geometry('400x500')
 tela.resizable(0,0)
 tela.title("Formulário")
 
-#Campo nome
-tNome = Label(tela, text="Nome",background="green").grid(row=0,column=0)
 
+#Campo: nome
+tNome = Label(tela,text="Insira um nome:").place(x=0,y=2)
+eNome = Entry(tela, width=30).place(x=0,y=40)
 
-
-
-
-
-
-
+#Campo: e-mail
+tEmail= Label(tela, text="Insira um e-mail:").place(x=0,y=90)
+eEmail= Entry(tela, width=30).place(x=0, y=130)
 
 
 tela.mainloop()
